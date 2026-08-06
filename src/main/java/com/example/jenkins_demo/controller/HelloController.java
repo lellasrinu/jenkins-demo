8fe8f31ebb01cf739741d164b8197659c6d1f833
Srinu Lella <lellasrinu@gmail.com>
2026-08-06 14:41:30 +0530

    updated

--- src/main/java/com/example/jenkins_demo/controller/HelloController.java
+++ src/main/java/com/example/jenkins_demo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World Jenkins Pipelines";
+        return "Hello World Jenkins Pipelines Hare Rama";
     }
 
     @GetMapping("/hi")
